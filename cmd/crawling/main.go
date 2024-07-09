66d3b513f097b6b8564bf21f7e6d8eb6eda9a2ae
X_V.neer <xv.neer@gmail.com>
2024-07-09 16:45:56 +0300

    feat: implement accuale crawling

--- cmd/crawling/main.go
+++ cmd/crawling/main.go
@@ -1,7 +1,17 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/x-vneer/crawling/internal/crawling"
+)
+
 func main() {
 
 	// TODO: Implement crawling
 
+	var crawledPages map[string]uint8 = make(map[string]uint8)
+	crawling.Crawling("https://mabet.com.sa", "https://mabet.com.sa/ar", &crawledPages)
+
+	fmt.Println(crawledPages)
 }
---
+++ internal/crawling/crawling.go
@@ -0,0 +1,44 @@
+package crawling
+
+import (
+	"fmt"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/x-vneer/crawling/internal/tools"
+)
+
+func Crawling(baseURL string, currentURL string, crawledPages *map[string]uint8) {
+
+	fmt.Printf("Crawling: %s", currentURL)
+
+	url := tools.NormalizeURL(currentURL)
+	fmt.Println(url)
+
+	if len(url) == 0 {
+		log.Warn("Empty url")
+		return
+	}
+
+	_, ok := (*crawledPages)[url]
+
+	(*crawledPages)[url] += 1
+
+	if ok {
+		return
+	}
+
+	page, err := tools.GetHTMLPage(currentURL)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	// Get all URLs from currentURL
+	urls := tools.GetURLsFromHTML(page, baseURL)
+
+	// Crawl each URL
+	for _, url := range urls {
+		Crawling(baseURL, url, crawledPages)
+	}
+
+}
